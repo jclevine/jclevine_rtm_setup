bcf0433b3f175cae316f411f7febd875f3559e7d
jlevine-loyalty <jlevine@loyalty.com>
2015-09-10 16:35:04 -0400

    Add some comments.

--- src/com/jlevine/seterup.py
+++ src/com/jlevine/seterup.py
@@ -3,9 +3,9 @@ import rtm
 # TODO: jlevine - Squash this commit and all the previous stupid Markdown issue commits together.
 #       I should be embarrassed.
 # TODO: jlevine - Maybe find a way to get these constants dynamically from the user on the command-line.
-APIKEY = '816d569942411f52e9c63268f2238ece'
-SHARED_SECRET = 'fcc2f9c553c64df6'
-TOKEN = '3b4ad5d73fd2213211653806629670ae5e8ea6ad'
+APIKEY = '#########################'
+SHARED_SECRET = '####################'
+TOKEN = '#######################'
 
 # TODO; jlevine - Move each section into a function.
 if __name__ == '__main__':
